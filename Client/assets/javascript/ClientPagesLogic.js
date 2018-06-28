89997ea05100e42040cea767b85358fefc7fd083
Mel Sidd <mlsiddoway@gmail.com>
2018-06-27 20:25:44 -0500

    updated some styling and some javascript to make sure buttons are clickable.  Need to still fix firebase

---
+++ Client/assets/javascript/ClientPagesLogic.js
@@ -0,0 +1,124 @@
+// IF user is going to create new account...
+//----------------if else statements will start here once incluce these features-----------------
+// Get the client's user name and password from the input boxes
+var clientUserName = "";
+var clientPassword = "";
+var clientFirst = "";
+var clientLast = "";
+var address = "";
+var address2 = "";
+var city = "";
+var state = "";
+var zip = "";
+var phone = "";
+var squareFootage = 0;
+var servicesNeeded = [];
+
+
+// IF user is going to use google account to sign up
+//-------------------enter code here for using Google API-------------------
+
+//IF user is going to use facebook to sign up
+//------------------enter code here for using facebook API--------------------------
+
+   // Capture Button Click
+$("#create_client_account").on("click", function(event) {
+    event.preventDefault();
+
+    // Get the user's data from the input boxes for Firebase database.
+    clientUserName = $("#email-input").val().trim();
+    clientPassword = $("#password-input").val().trim();
+    // form validation
+    if(clientUserName.indexOf("@") == -1 && clientPassword.length < 7) {
+        $("#data-validation-message").html("Make sure you are entering your email <p> Password must contain at least 7 characters");        
+    } else if (clientPassword.length < 7) {
+        $("#data-validation-message").text("Password must contain at least 7 characters");
+    } else if (clientUserName.indexOf("@") == -1) {
+        $("#data-validation-message").text("Make sure you are entering your email");
+    } else if(clientUserName.indexOf("@") > 0 && clientPassword.length > 6) {
+        window.location.href = "./Register-Client.html";
+    }
+    console.log(clientUserName);
+    console.log(clientPassword);
+});
+
+   // Capture Button Click
+   $("#create-client-profile").on("click", function(event) {
+    event.preventDefault();
+
+    // Get values from input boxes
+    clientFirst = $("#clientFirstName").val().trim();
+    clientLast = $("#clientLastName").val().trim();
+    address = $("#inputAddress").val().trim();
+    address2 = $("#inputAddress2").val().trim();
+    city = $("#inputCity").val().trim();
+    state = $("#inputState").val().trim();
+    zip = $("#inputZip").val().trim();
+    phone = $("#clientPhoneNumber").val().trim();
+    squareFootage = $("#squareFootage").val().trim();
+    servicesNeeded1 = $("#mowing").val();
+    servicesNeeded2 = $("#edging").val();
+    servicesNeeded3 = $("#trimBushes").val();
+
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDFhTiBSjTaFH-bYZdHI5v_FRS3nE76adk",
+    authDomain: "thp1g1-1529168178742.firebaseapp.com",
+    databaseURL: "https://thp1g1-1529168178742.firebaseio.com",
+    projectId: "thp1g1-1529168178742",
+    storageBucket: "thp1g1-1529168178742.appspot.com",
+    messagingSenderId: "293280499251"
+  };
+  firebase.initializeApp(config);
+  
+  var dataRef = firebase.database();
+
+    // Push client data to Firebase
+    dataRef.ref().push({
+    
+    usertype: "client",
+    username: clientUserName,
+    password: clientPassword,
+    firstname: clientFirst,
+    lastname: clientLast,
+    address: address,
+    address2: address2,
+    state: state,
+    city: city,
+    zip: zip,
+    phone: phone,
+    sqft: squareFootage,
+    servicesneeded: [servicesNeeded1, servicesNeeded2, servicesNeeded3],
+    //coordinateslat: coordinatesLat,
+    //coordianteslong: coordinatesLong, 
+    //calendar: , //need to figure this out
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+    if (clientFirst.length < 1) {
+        $("#data-validation-message-registration").append("Enter your first name");
+    } if (clientLast.length < 1) {
+        $("#data-validation-message-registration").append("<p> Enter your last name");
+    } if(address.length < 1) {
+        $("#data-validation-message-registration").append("<p> Enter your address");
+    } if(state.length < 3) {
+        $("#data-validation-message-registration").append("<p> Make sure you select your state");
+    } if(city.length < 1) {
+        $("#data-validation-message-registration").append("<p> Make sure you select your city");
+    } if(zip.length < 5 || zip.length > 5) {
+        $("#data-validation-message-registration").append("<p> Enter a valid zip code");
+    } if(phone.length < 10 || phone.length > 10) {
+        $("#data-validation-message-registration").append("<p> Enter a valid phone number");
+    } if(squareFootage.length < 1) {
+        $("#data-validation-message-registration").append("<p> Make sure you enter the square footage of your lawn");
+    } if($("#mowing").prop('checked') == false && $("#trimBushes").prop('checked') == false && $("#edging").prop('checked') == false) {
+        $("#data-validation-message-registration").append("Make sure to select at least 1 service");
+    } else if(clientFirst.length > 1 && clientLast.length > 1 && address.length > 1 && state.length > 3 && city.length > 1 && zip.length == 5 && phone.length == 10 && squareFootage.length > 1 && ($("#mowing").prop('checked') == true || $("#edging").prop('checked') == true || $("#trimBushes").prop('checked') == true)) {
+        window.location.href = "./ClientLandingPage.html";
+    }
+
+    console.log(clientFirst);
+    console.log(servicesNeeded1, servicesNeeded2,  servicesNeeded3)
+   });
---
+++ Provider/assets/javascript/ProviderPagesLogic.js
@@ -0,0 +1,282 @@
+// Declare variables
+var providerUserName = "";
+var providerPassword = "";
+var providerFirst = "";
+var providerLast = "";
+var businessName = "";
+var providerAddress = "";
+var providerAddress2 = "";
+var providerCity = "";
+var providerState = "";
+var providerZip = "";
+var providerPhone = "";
+var providerMiles = 0;
+var servicesprovided = [];
+var availableDays = [];
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDFhTiBSjTaFH-bYZdHI5v_FRS3nE76adk",
+    authDomain: "thp1g1-1529168178742.firebaseapp.com",
+    databaseURL: "https://thp1g1-1529168178742.firebaseio.com",
+    projectId: "thp1g1-1529168178742",
+    storageBucket: "thp1g1-1529168178742.appspot.com",
+    messagingSenderId: "293280499251"
+  };
+  firebase.initializeApp(config);
+    
+  var dataRef = firebase.database();
+
+  console.log(dataRef);
+
+
+   //-------------LOGIN PAGE CODE IS BELOW-------------//
+
+//-------------------enter code here for using Google API-------------------
+
+//------------------enter code here for using facebook API--------------------------
+
+// Event listener tied to create account button to store username and password data 
+$("#create_provider_account").on("click", function(event) {
+    event.preventDefault();
+    console.log(dataRef);
+
+
+    // Get the user's username and password from the input boxes for Firebase database.
+    providerUserName = $("#email-input").val().trim();
+    providerPassword = $("#password-input").val().trim();
+    // login form validation to ensure using email address and password is at least 6 characters long
+    if(providerUserName.indexOf("@") == -1 && providerPassword.length < 6) {
+        $("#data-validation-message").html("Make sure you are entering your email <p> Password must contain at least 6 characters");        
+    } else if (providerPassword.length < 6) {
+        $("#data-validation-message").text("Password must contain at least 6 characters");
+    } else if (providerUserName.indexOf("@") == -1) {
+        $("#data-validation-message").text("Make sure you are entering your email");
+    } else if(providerUserName.indexOf("@") > 0 && providerPassword.length > 5) {
+        // Create a new user account and store it using local storage
+        sessionStorage.setItem("userName", providerUserName);
+        sessionStorage.setItem("Password", providerPassword);
+
+        var ref = firebase.database().ref("providers/username");
+        ref.once("value")
+        .then(function(snapshot) {
+        snapshot.exists();  // true
+        });
+
+            // check firebase to see if the username/password already exists
+            // // Fetch a Blog Post by ID. Returns a Promise of an actual object, not a DataSnapshot.
+            // function getUserFromDatabase(currentusername) {
+            //     return dataRef.child('providers').child(currentusername).once('value').then(function(snapshot) {
+            //     return snapshot.val();
+            //     });
+            // }
+            // console.log(getUserFromDatabase(providerUserName));
+            //     // if (snapshot.child.val().username !== providerUserName) {
+            //     //     //push username and password to firebase database if username is available
+                //     dataRef.ref('providers/').push({ 
+                //         username: providerUserName,
+                //         password: providerPassword });
+                //         // AND take user to registration page to complete registration
+                //         window.location.href = "./Registration-Provider.html";
+                // }
+                // else {
+                // // return message if username already exists
+                // $("#data-validation-message").text("That username and/or password already exists.");
+                // }    
+        }
+            });
+    //     }
+    // });
+
+   //-------------REGISTRATION PAGE CODE IS BELOW-------------//
+
+   // Capture Button Click
+   $("#create-provider-profile").on("click", function(event) {
+    event.preventDefault();
+    
+    // Get values from input boxes
+    providerFirst = $("#providerFirstName").val().trim();
+    providerLast = $("#providerLastName").val().trim();
+    businessName = $("#businessName").val().trim();
+    providerAddress = $("#inputAddress").val().trim();
+    providerAddress2 = $("#inputAddress2").val().trim();
+    providerCity = $("#inputCity").val().trim();
+    providerState = $("#inputState").val().trim();
+    providerZip = $("#inputZip").val().trim();
+    providerPhone = $("#providerPhoneNumber").val().trim();
+    providerMiles = $("#miles").val().trim();
+    //get the value only if the user checked the radio button
+    if ($("#trimBushes").prop("checked")) {
+        servicesprovided.push($("#trimBushes").val())
+    }
+    if ($("#edging").prop("checked")) {
+        servicesprovided.push($("#edging").val())
+    }
+    if ($("#mowing").prop("checked")) {
+        servicesprovided.push($("#mowing").val())
+    }
+    if ($("#monday").prop("checked")) {
+        availableDays.push($("#monday").val())
+    }
+    if ($("#tuesday").prop("checked")) {
+        availableDays.push($("#tuesday").val())
+    }
+    if ($("#wednesday").prop("checked")) {
+        availableDays.push($("#wednesday").val())
+    }
+    if ($("#thursday").prop("checked")) {
+        availableDays.push($("#thursday").val())
+    }
+    if ($("#friday").prop("checked")) {
+        availableDays.push($("#friday").val())
+    }
+    if ($("#saturday").prop("checked")) {
+        availableDays.push($("#saturday").val())
+    }
+    if ($("#sunday").prop("checked")) {
+        availableDays.push($("#sunday").val())
+    }
+    //data validation to ensure user entered information correctly
+    if (providerFirst.length < 1) {
+        $("#data-validation-message-registration").append("Enter your first name");
+    } if (providerLast.length < 1) {
+        $("#data-validation-message-registration").append("<p> Enter your last name");
+    } if(providerAddress.length < 1) {
+        $("#data-validation-message-registration").append("<p> Enter your address");
+    } if(providerState.length < 3) {
+        $("#data-validation-message-registration").append("<p> Make sure you select your state");
+    } if(providerCity.length < 1) {
+        $("#data-validation-message-registration").append("<p> Make sure you select your city");
+    } if(providerZip.length < 5 || providerZip.length > 5) {
+        $("#data-validation-message-registration").append("<p> Enter a valid zip code");
+    } if(providerPhone.length < 10 || providerPhone.length > 10) {
+        $("#data-validation-message-registration").append("<p> Enter a valid phone number");
+    } if(providerMiles.length < 3) {
+        $("#data-validation-message-registration").append("<p> Make sure you enter the amount of miles you are willing to travel");
+    } if($("#monday").prop('checked') == false && $("#tuesday").prop('checked') == false && $("#wednesday").prop('checked') == false && $("#thursday").prop('checked') == false && $("#friday").prop('checked') == false && $("#saturday").prop('checked') == false && $("#sunday").prop('checked') == false) {
+        $("#data-validation-message-registration").append("Make sure to select at least 1 day your are available"); 
+    } if($("#mowing").prop('checked') == false && $("#trimBushes").prop('checked') == false && $("#edging").prop('checked') == false) {
+        $("#data-validation-message-registration").append("Make sure to select at least 1 service");
+    } else if(providerFirst.length > 1 && providerLast.length > 1 && providerAddress.length > 1 && providerState.length > 3 && providerCity.length > 1 && providerZip.length == 5 && providerPhone.length == 10 && providerMiles.length > 1 && ($("#mowing").prop('checked') == true || $("#edging").prop('checked') == true || $("#trimBushes").prop('checked') == true) && ($("#monday").prop('checked') == true || $("#tuesday").prop('checked') == true || $("#wednesday").prop('checked') == true || $("#thursday").prop('checked') == true || $("#friday").prop('checked') == true || $("#saturday").prop('checked') == true || $("#sunday").prop('checked') == true)) {
+
+        // Push provider data to THE SAME user in the database
+        dataRef.ref('providers/').push({
+            username: sessionStorage.getItem("userName"),
+            password: sessionStorage.getItem("Password"),
+            firstname: providerFirst,
+            lastname: providerLast,
+            businessName: businessName,
+            address: providerAddress,
+            address2: providerAddress2,
+            state: providerState,
+            city: providerCity,
+            zip: providerZip,
+            phone: providerPhone,
+            miles: providerMiles,
+            availableDays: availableDays,
+            servicesprovided: servicesprovided,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        // Take user to the landing page    
+        window.location.href = "./ProviderLandingPage.html";
+    };
+
+   });
+
+   //-------------RETURNING USERS CODE IS BELOW-------------//
+   //need to add event listener to login button for returing logins and then user is taken direction to landing page
+  
+
+   //-------------LANDING PAGE CODE IS BELOW-------------//
+
+   dataRef.ref().on("child_added", function(childSnapshot) {
+       console.log(childSnapshot.val());
+
+       $("#firstNameStored").text(childSnapshot.val().providerFirst);
+
+   }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
+
+   //Store each button in a variable
+   var profile = $("#profile");
+   var calendar = $("#calendar");
+   var inbox = $("#inbox");
+//    var payments = $("#payments");
+//    var jobHistory = $("#jobHistory");
+//    var requests = $("#requests");
+//    var upcomingJobs = $("#upcomingJobs");
+   var placeholderForDisplay = $("#placeholderForLandingPageDisplay");
+   var profileHolder = $("#holderForProfileDisplay");
+   var inboxHolder = $("#placeholderForIndexDisplay");
+   var calendarHolder = $("#placeholderForCalendarDisplay");
+//    var requestsHolder = $("#holderForRequestsDisplay");
+
+
+   // When User clicks panel button, display relevant information
+    profile.on("click", function(event) {
+        event.preventDefault();
+        profileHolder.show();
+        placeholderForDisplay.hide();
+        inboxHolder.hide();
+        calendarHolder.hide();
+        requestsHolder.hide();
+        inboxHolder.hide();
+    })
+
+    // requests.on("click", function(event) {
+    //     event.preventDefault();
+    //     requestsHolder.show();
+    //     placeholderForDisplay.hide();
+    //     inboxHolder.hide();
+    //     calendarHolder.hide();
+    //     profileHolder.hide();
+    //     inboxHolder.hide();
+    //     })
+
+    inbox.on("click", function(event) {
+        event.preventDefault();
+        inboxHolder.show();
+        requestsHolder.hide();
+        placeholderForDisplay.hide();
+        inboxHolder.hide();
+        calendarHolder.hide();
+        profileHolder.hide();
+        })
+
+    calendar.on("click", function(event) {
+        event.preventDefault();
+        calendarHolder.show();
+        requestsHolder.hide();
+        placeholderForDisplay.hide();
+        inboxHolder.hide();
+        requestsHolder.hide();
+        profileHolder.hide();
+        })
+
+    // payments.on("click", function(event) {
+    //     event.preventDefault();
+    //     paymentsHolder.show();
+    //     requestsHolder.hide();
+    //     placeholderForDisplay.hide();
+    //     inboxHolder.hide();
+    //     calendarHolder.hide();
+    //     profileHolder.hide();
+    //     })    
+
+
+       //capture the updated values stored in firebase from the logged in user
+//    database.ref().on("value", function(snapshot) {
+
+//     $("#employee-table").append("<td>").text(snapshot.child("EmployeeName").val());
+
+
+// console.log(snapshot.val());
+// })
+
+
+
+
+
+
+    
